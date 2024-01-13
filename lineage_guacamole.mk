484536b975301475068f31bf25a9f04b8f33814a
mnrniloy <mnrniloy0@gmail.com>
2024-01-13 12:16:02 +0600

    guacamole: Project Matrixx Adaptation

--- lineage_guacamole.mk
+++ lineage_guacamole.mk
@@ -11,11 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Matrixx
+MATRIXX_BUILD_TYPE := UnOfficial
+MATRIXX_MAINTAINER := NILOY
+MATRIXX_CHIPSET := SM8150
+MATRIXX_BATTERY := 4085mAh
+MATRIXX_DISPLAY := 1440x3120
+WITH_GMS := true
+TARGET_HAS_UDFPS := true
+TARGET_DISABLE_EPPE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_guacamole
+PRODUCT_NAME := lineage_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := GM1917
@@ -32,6 +42,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 scr_resolution := 1440
 TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Build Fingerprint
 BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:11/RKQ1.201022.002/2206171030:user/release-keys"
