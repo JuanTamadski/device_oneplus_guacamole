3afbc9de6475b0a5318f55784c6628fba7ca1e00
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2024-01-23 18:11:57 +0800

    Guacamole: renaming stuffs for superior bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_guacamole.mk
+    $(LOCAL_DIR)/superior_guacamole.mk
 
 COMMON_LUNCH_CHOICES := \
-	yaap_guacamole-user \
-	yaap_guacamole-userdebug \
-	yaap_guacamole-eng
+	superior_guacamole-user \
+	superior_guacamole-userdebug \
+	superior_guacamole-eng
